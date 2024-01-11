96cf41195cde42bf054eda11f610c4c3eb463822
0x0606FB <trapezanlidisn@gmail.com>
2024-01-11 19:51:51 +0200

    lol

--- src/professor.cpp
+++ src/professor.cpp
@@ -1,4 +1,5 @@
 #include "./../headers/professor.hpp"
+#include <memory>
 
 Professor::Professor(){};
 Professor::~Professor(){};
@@ -10,4 +11,9 @@ void Professor::assign_to_course(std::shared_ptr<Course> c){
 
 void Professor::drop_course(std::shared_ptr<Course> c){
     this->courselist.remove(c);
+}
+
+void Professor::grade_student(std::shared_ptr<Student>, std::shared_ptr<Course>) {
+
+
 }
